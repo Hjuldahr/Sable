33246a1684af79e52a0eb3b589e7a6cb6adbcc59
Hjuldahr <rjohly@gmail.com>
2026-01-21 12:40:47 -0500

    Migrating to databases from raw aiosqlite

--- components/db/database.py
+++ components/db/database.py
@@ -1,71 +1,46 @@
+import asyncio
 from pathlib import Path
-
-import aiosqlite
+from databases import Database
 from loguru import logger
 
 class DatabaseManager:
+    DB_URL = "sqlite+aiosqlite:///./data/db/database.db"
+    
     PATH_ROOT = Path(__file__).resolve().parents[2]
     PRAGMA_SCRIPT_PATH = PATH_ROOT / 'components' / 'db' / 'pragma.sqlite'
     SETUP_SCRIPT_PATH = PATH_ROOT / 'components' / 'db' / 'setup.sqlite'
     DB_PATH = PATH_ROOT / 'data' / 'sqlite' / 'database.db'
     
-    db: aiosqlite.Connection = None
+    def __init__(self):
+        self.db: Database = Database(self.DB_URL)
     
-    # Call only once per application
-    @classmethod
-    async def async_init(cls):
-        db = await cls.conn()
-        await db.executescript(cls.SETUP_SCRIPT_PATH.read_text())
-        await db.commit()
-            
-    @classmethod
-    def _is_db_running(cls) -> bool:
-        return getattr(cls.db, "_running", False)       
-            
-    @classmethod
-    async def connect(cls) -> aiosqlite.Connection:
-        if cls.db is None or not cls._is_db_running():
-            cls.db = await aiosqlite.connect(cls.DB_PATH, timeout=10)
-            await cls.db.executescript(cls.PRAGMA_SCRIPT_PATH.read_text())
-            logger.info("Connected to SQLite")
-            
-        return cls.db
-    
-    @classmethod
-    async def disconnect(cls):
-        if cls.db is not None and cls._is_db_running():
-            await cls.db.close()
-            cls.db = None
-            logger.info(f"Disconnected from SQLite")
+    async def _async_init(self):
+        await self.db.connect()
+        await self.db.execute(self.PRAGMA_SCRIPT_PATH)
         
-    async def _rollback(self):
-        try:
-            if self.db.in_transaction:
-                await self.db.rollback()
-                logger.info("Transaction rollback successful")
-        except Exception as e:
-            logger.exception(f"Transaction rollback failed: {e}")
+    async def _async_close(self):
+        await self.db.disconnect()
     
     # ---- AI Profile ----
-    @classmethod
-    async def select_ai_persona(cls) -> dict:
-        pass
+    async def select_ai_persona(self) -> dict:
+        async with self.db.transaction():
+            await self.db.execute('')
     
-    @classmethod
-    async def update_ai_persona(cls):
+    async def update_ai_persona(self):
         pass
     
     # ---- AI Memories ----
-    @classmethod
-    async def upsert_ai_memory(cls):
+    async def upsert_ai_memory(self):
         pass
     
-    @classmethod
-    async def read_ai_memories(cls) -> list[dict]:
+    async def read_ai_memories(self) -> list[dict]:
         pass
     
-    @classmethod
-    async def delete_old_ai_memories(cls):
+    async def delete_old_ai_memories(self):
         pass
     
     # ---- User Memories ----
+    
+if __name__ == '__main__':
+    test = DatabaseManager()
+    asyncio.run(test.async_init())   
